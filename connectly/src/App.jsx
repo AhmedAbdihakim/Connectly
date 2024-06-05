bf8572046b6e32b35416d3ac19fdd49c6878433f
Ahmed Abdihakim <ahmedabdihakim02@gmail.com>
2024-06-05 17:00:35 +0300

    Set up the app routes

--- connectly/src/App.jsx
+++ connectly/src/App.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Feed from "./components/Feed";
+import Login from "./components/Login";
+import Post from "./components/Post";
+import Profile from "./components/Profile";
+import MyPosts from "./components/MyPosts";
+import Navbar from "./components/Navbar";
+import NewPost from "./components/NewPost";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Posts/:postId" element={<Post />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/myPosts" element={<MyPosts />} />
+        <Route path="/newPost" element={<NewPost />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
